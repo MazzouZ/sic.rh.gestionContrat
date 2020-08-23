6dac55d751c3ac1f2e6bed5262ecf85d50ae2eb9
Abounki Hamza <abounkihamza@gmail.com>
2020-08-23 16:03:03 +0100

    servce updte

--- src/main/java/ma/sic/rh/gestionContrat/services/MyRepositoryRestConfigurerAdapter.java
+++ src/main/java/ma/sic/rh/gestionContrat/services/MyRepositoryRestConfigurerAdapter.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
+import ma.sic.rh.gestionContrat.entities.DelegateReference;
 import ma.sic.rh.gestionContrat.entities.Manager;
 import ma.sic.rh.gestionContrat.entities.SDLReference;
 @Configuration
 public class MyRepositoryRestConfigurerAdapter implements RepositoryRestConfigurer{
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(SDLReference.class,Manager.class);
+        config.exposeIdsFor(SDLReference.class,Manager.class,DelegateReference.class);
     }
 }
