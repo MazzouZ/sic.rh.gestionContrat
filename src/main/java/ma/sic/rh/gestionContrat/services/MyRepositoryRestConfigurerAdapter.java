46b7a042a77f5ea049d9fbb61aaf86bf6447bcd9
Abounki Hamza <abounkihamza@gmail.com>
2020-10-08 16:17:27 +0100

    Inv update / service update

--- src/main/java/ma/sic/rh/gestionContrat/services/MyRepositoryRestConfigurerAdapter.java
+++ src/main/java/ma/sic/rh/gestionContrat/services/MyRepositoryRestConfigurerAdapter.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.sun.xml.txw2.Document;
+import ma.sic.rh.gestionContrat.entities.Document;
 
+import ma.sic.rh.gestionContrat.entities.Contrat;
 import ma.sic.rh.gestionContrat.entities.DelegateReference;
+import ma.sic.rh.gestionContrat.entities.Investissement;
 import ma.sic.rh.gestionContrat.entities.Manager;
 import ma.sic.rh.gestionContrat.entities.SDLReference;
 @Configuration
 public class MyRepositoryRestConfigurerAdapter implements RepositoryRestConfigurer{
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(SDLReference.class,Manager.class,DelegateReference.class);
+        config.exposeIdsFor(SDLReference.class,Manager.class,DelegateReference.class,Contrat.class,Investissement.class,Document.class);
     }
 }
